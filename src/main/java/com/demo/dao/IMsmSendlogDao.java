5622f054bcc2f9173077257ddc876d57c666641b
wangzaijun <wzj@simpletour.com>
2016-07-28 16:08:44 +0800

    mybatis 的基础Dao接口写完了

---
+++ src/main/java/com/demo/dao/IMsmSendlogDao.java
@@ -0,0 +1,11 @@
+package com.demo.dao;
+
+import com.demo.domain.MsmSendlog;
+import com.demo.query.BaseQuery;
+
+/**
+ * Created by simpletour_Jenkin on 2016/7/28.
+ */
+public interface IMsmSendlogDao extends IDao<MsmSendlog,BaseQuery> {
+
+}
