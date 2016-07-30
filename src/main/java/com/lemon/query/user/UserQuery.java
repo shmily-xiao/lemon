7e07f9d611cd5fc3ccb16b4ba15674aabc700ab1
wangzaijun <wzj@simpletour.com>
2016-07-30 16:37:10 +0800

    user 的基础底层基本写完，但是分页还没有完善

---
+++ src/main/java/com/lemon/query/user/UserQuery.java
@@ -0,0 +1,193 @@
+package com.lemon.query.user;
+
+import com.lemon.query.BaseQuery;
+
+import java.util.Date;
+
+/**
+ * Created by simpletour_Jenkin on 2016/7/30.
+ */
+public class UserQuery extends BaseQuery {
+
+    /**
+     * 头像
+     */
+    private String avatar;
+
+    /**
+     * 性别
+     */
+    private String gender;
+
+    /**
+     * 名字
+     */
+    private String name;
+
+    /**
+     * 昵称
+     */
+    private String nickName;
+
+    /**
+     * 生日
+     */
+    private Date birthday;
+
+    /**
+     * 电话号码
+     */
+    private String mobile;
+
+    /**
+     * qq号码
+     */
+    private String qqNo;
+
+    /**
+     * 邮箱
+     */
+    private String email;
+
+    /**
+     * 密码（MD5)
+     */
+    private String password;
+
+    /**
+     * 随机数
+     */
+    private String salt;
+
+    /**
+     * 状态
+     */
+    private String status;
+
+    /**
+     * 用户的类型，或者是等级
+     */
+    private String type;
+
+    /**
+     * 成就值，得分
+     */
+    private Long score;
+
+    /**
+     * 用户是否公开自己的事件，或者是只能好友可见
+     */
+    private String zoneStatus;
+
+    public String getAvatar() {
+        return avatar;
+    }
+
+    public void setAvatar(String avatar) {
+        this.avatar = avatar;
+    }
+
+    public Date getBirthday() {
+        return birthday;
+    }
+
+    public void setBirthday(Date birthday) {
+        this.birthday = birthday;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public String getMobile() {
+        return mobile;
+    }
+
+    public void setMobile(String mobile) {
+        this.mobile = mobile;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getNickName() {
+        return nickName;
+    }
+
+    public void setNickName(String nickName) {
+        this.nickName = nickName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getQqNo() {
+        return qqNo;
+    }
+
+    public void setQqNo(String qqNo) {
+        this.qqNo = qqNo;
+    }
+
+    public String getSalt() {
+        return salt;
+    }
+
+    public void setSalt(String salt) {
+        this.salt = salt;
+    }
+
+    public Long getScore() {
+        return score;
+    }
+
+    public void setScore(Long score) {
+        this.score = score;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getZoneStatus() {
+        return zoneStatus;
+    }
+
+    public void setZoneStatus(String zoneStatus) {
+        this.zoneStatus = zoneStatus;
+    }
+}
