9916c16a7c979cce0709bc0af5964cfa6204abff
wangzaijun <wzj@simpletour.com>
2016-08-19 09:54:15 +0800

    数据库变更,导致数据结构的变动

---
+++ src/main/java/com/lemon/domain/friend/FriendGroup.java
@@ -0,0 +1,41 @@
+package com.lemon.domain.friend;
+
+import com.lemon.domain.BaseDomain;
+import com.lemon.domain.interfaces.friend.IFriendGroup;
+import com.lemon.enums.FriendType;
+
+/**
+ * Created by simpletour_Jenkin on 2016/8/19.
+ */
+public class FriendGroup extends BaseDomain implements IFriendGroup{
+
+    /**
+     * 用户的id
+     */
+    private Long userId;
+
+    /**
+     * 用户的分组名字
+     */
+    private String groupName;
+
+    @Override
+    public String getGroupName() {
+        return groupName;
+    }
+
+    @Override
+    public void setGroupName(String groupName) {
+        this.groupName = groupName;
+    }
+
+    @Override
+    public Long getUserId() {
+        return userId;
+    }
+
+    @Override
+    public void setUserId(Long userId) {
+        this.userId = userId;
+    }
+}
---
+++ src/main/java/com/lemon/domain/user/UserRecord.java
@@ -0,0 +1,86 @@
+package com.lemon.domain.user;
+
+import com.lemon.domain.BaseDomain;
+import com.lemon.domain.interfaces.user.IUserRecord;
+import com.lemon.enums.UserType;
+
+/**
+ * Created by simpletour_Jenkin on 2016/8/19.
+ */
+public class UserRecord extends BaseDomain implements IUserRecord {
+    /**
+     * 用户的等级
+     */
+    private UserType type;
+
+    /**
+     * 用户的成就值
+     */
+    private Long score;
+
+    /**
+     * 短信的条数
+     */
+    private Long smsCount;
+
+    /**
+     * 用户的id
+     */
+    private Long userId;
+
+    /**
+     * 用户对好友的公开策略
+     */
+    private Long accessControlId;
+
+
+    @Override
+    public Long getAccessControlId() {
+        return accessControlId;
+    }
+
+    @Override
+    public void setAccessControlId(Long accessControlId) {
+        this.accessControlId = accessControlId;
+    }
+
+    @Override
+    public Long getScore() {
+        return score;
+    }
+
+    @Override
+    public void setScore(Long score) {
+        this.score = score;
+    }
+
+    @Override
+    public Long getSmsCount() {
+        return smsCount;
+    }
+
+    @Override
+    public void setSmsCount(Long smsCount) {
+        this.smsCount = smsCount;
+    }
+
+    @Override
+    public UserType getType() {
+        return type;
+    }
+
+    @Override
+    public void setType(UserType type) {
+        this.type = type;
+    }
+
+    @Override
+    public Long getUserId() {
+        return userId;
+    }
+
+    @Override
+    public void setUserId(Long userId) {
+        this.userId = userId;
+    }
+}
